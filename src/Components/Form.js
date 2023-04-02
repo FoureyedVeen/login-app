ac4be5f3f3b88906c9c8ba841c7909ed1f9fb9a7
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-04-02 09:41:18 +0100

    Transition

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,8 +3,17 @@ import './Form.css'
 export default function Form () {
     return (
         <>
-        <form>
-            
+        <form className='form'>
+            <div className='user-box'>
+                <input type={"text"} name = {"username"} for={""}></input>
+                <label>Username</label>
+            </div>
+            <div className='user-box'>
+                <input type={"text"} name = {"password"} for={""}></input>
+                <label>Password</label>
+            </div>
+            <div></div>
+            <div></div>
         </form>
         </>
     )
