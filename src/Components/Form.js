249684397223bdec7b971272ad2e10de7471f498
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-04-20 23:32:04 +0100

    Feat: Sign up box

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,22 +1,25 @@
 import './Form.css'
 
-export default function Form () {
+function Form () {
     return (
         <>
         <form className='form'>
             <div className='user-box'>
                 <input type={"text"} placeholder={""}></input>
-                <label>Username</label>
+                <label>Username / Email</label>
             </div>
             <div className='user-box'>
                 <input type={"text"} name = {"password"} for={""} placeholder={""}></input>
                 <label>Password</label>
             </div>
-            <div>
-                <button className='button'>Sign in</button>
+            <div className='button-container'>
+                <button className='button'>Login</button>
             </div>
-            <div></div>
+            <div className='signup-box'>
+                <p className='signup-text'>Not a member ? <a href="/signup" className='signup-link'>Sign up</a></p>
+            </div>
         </form>
         </>
     )
 }
+export default Form;
