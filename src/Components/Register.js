feb26ccb40dd58437310ab1fb421ac241b711195
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-04-20 23:32:42 +0100

    Register component

---
+++ src/Components/Register.js
@@ -0,0 +1,17 @@
+import "./Form.css"
+
+
+
+function Register() {
+    return (
+        <>
+            <form>
+                <div className="name-box">
+                    <input type="text">First Name</input>
+                    <input type="text">Last Name</input>
+                </div>
+            </form>
+        </>
+    )
+}
+export default Register;
