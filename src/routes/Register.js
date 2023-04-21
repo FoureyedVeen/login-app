d609b9d9bcb78384a7a0493d2bb2cdc32563b5f8
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-04-21 22:01:45 +0100

    Feat: Sign up UI and styling

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,22 +1,36 @@
 import "../Components/Register.css"
 
 
-
-
-
 function Register() {
     return (
         <>
-            <div className="register-container">
-                <form className="">
-                    <div className="name-box">
-                        <p>Hello world</p>
-                        <input type="text" placeholder={""} />
-                        <input type="text" placeholder={""} />
-                    </div>
-                </form>
+            <div className="container">
+                <div className="login-box">
+                    <h2 className="login">SIGN UP</h2>
+                    <form className="form">
+                        <div className="user-box">
+                            <input type="text" placeholder={""} className="input"/>
+                            <label>First Name</label>
+                        </div>
+                        <div className="user-box">
+                            <input type="text" placeholder={""} className="input"/>
+                            <label>Last Name</label>
+                        </div>
+                        <div className="user-box">
+                            <input type="text" placeholder={""} className="input"/>
+                            <label>Email</label>
+                        </div>
+                        <div className="user-box">
+                            <input type="text" placeholder={""} className="input"/>
+                            <label>Password</label>
+                        </div>
+                        <div>
+                            <p className="signup-text">Already a member? <a href={`/`} className="signup-link">Log In</a></p>
+                            <button className="button">Sign Up</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            
         </>
     )
 }
