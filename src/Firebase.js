2349827e260566b5df8b5beac4abb64819f450c8
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-04-22 21:19:38 +0100

    Tried out realtime database didn't post the details

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,11 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyAhMpUv8BTGYykKIuBF8GiVrmHtp-D8ifk",
   authDomain: "loginauth-f5875.firebaseapp.com",
@@ -16,9 +14,16 @@ const firebaseConfig = {
   measurementId: "G-B9QYLF8PCC"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+
+
 
 // Initialize FirebaseAuth and get a reference to the service
-const auth = getAuth(app);
+export const auth = getAuth(app);
+
+export const database = getDatabase(app);
+
+export default app;
--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,7 +1,39 @@
-import "../Components/Register.css"
+import "../Components/Register.css";
+import { useState} from "react";
+import {auth} from "../firebase"
 
+
+// src/firebase.js
 
 function Register() {
+    const [firstName, setFirstName] = useState(null);
+    const [lastName, setLastName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    const handleInputChange = (e) => {
+        const {id, value} = e.target;
+        if(id === "firstName"){
+            setFirstName(value);
+        }
+        if(id === "lastName"){
+            setLastName(value);
+        }
+        if(id === "email"){
+            setEmail(value);
+        }
+        if(id === "password"){
+            setPassword(value);
+        }
+    }
+
+    
+    const handleSubmit = (userId,firstName,lastName,email,password) => {
+           
+    
+          
+        
+    }
     return (
         <>
             <div className="container">
@@ -9,24 +41,36 @@ function Register() {
                     <h2 className="login">SIGN UP</h2>
                     <form className="form">
                         <div className="user-box">
-                            <input type="text" placeholder={""} className="input"/>
+                            <input type="text" placeholder={""} 
+                                className="input" id="firstName"
+                                value={firstName || ''} onChange={(e)=> handleInputChange(e)}
+                            />
                             <label>First Name</label>
                         </div>
                         <div className="user-box">
-                            <input type="text" placeholder={""} className="input"/>
+                            <input type="text" placeholder={""} 
+                                className="input" id="lastName"
+                                value={lastName || ''} onChange={(e)=> handleInputChange(e)}
+                            />
                             <label>Last Name</label>
                         </div>
                         <div className="user-box">
-                            <input type="text" placeholder={""} className="input"/>
+                            <input type="email" placeholder={""} 
+                                className="input" id="email"
+                                value={email || ''} onChange={(e)=> handleInputChange(e)}
+                            />
                             <label>Email</label>
                         </div>
                         <div className="user-box">
-                            <input type="text" placeholder={""} className="input"/>
+                            <input type="password" placeholder={""} 
+                                className="input" id="password"
+                                value={password || ''} onChange={(e)=> handleInputChange(e)}
+                            />
                             <label>Password</label>
                         </div>
                         <div>
                             <p className="signup-text">Already a member? <a href={`/`} className="signup-link">Log In</a></p>
-                            <button className="button">Sign Up</button>
+                            <button className="button" onClick={()=> handleSubmit()}>Sign Up</button>
                         </div>
                     </form>
                 </div>
