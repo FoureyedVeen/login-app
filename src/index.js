dace766370e173b0bc796940a35e959a792f6c0d
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-12-10 21:18:47 +0100

    Clean ups

--- src/index.js
+++ src/index.js
@@ -3,36 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-// import Register from "../src/routes/Register"
-// import Home from './routes/Home';
 
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
-//   {
-//     path: "/register",
-//     element: <Register />,
-//   },
-//   {
-//     path: "/home",
-//     element: <Home/>,
-//   },
-// ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
-     {/* <RouterProvider
-      router={router}
-    /> */}
   </React.StrictMode>
 );
 
